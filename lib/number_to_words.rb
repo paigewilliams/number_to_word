18e7adc31ae2148b8143cd0f6e77c759aece9315
Daniel Bennett and Paige Williams <paige@>
2018-11-14 15:41:12 -0800

    add more numbers to output

--- lib/number_to_words.rb
+++ lib/number_to_words.rb
@@ -4,11 +4,33 @@ ones = { 0 => 'zero', 1 => 'one', 2 => 'two', 3  => 'three', 4 => 'four', 5 => '
 
 teens = Hash.new()
 teens = { 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen' }
+tens = Hash.new()
+tens = {2 => 'twenty', 3 => 'thirty' }
 
 if number <= 10
 ones.fetch(number)
-elsif number > 10
+
+elsif number > 10 && number <= 19
 teens.fetch(number)
+
+elsif number > 20
+   array = number.to_s.split("")
+   number_array = array.map(&:to_i)
+   number_array.each do |digit|
+    if number_array[0] == 2 && number_array[1] < 10
+      replaced = number_array[0].to_s.replace "twenty" + ones.fetch(number_array[1])
+      #&& number_array[1].to_s.replace "one"
+    end
+    binding.pry
+  end
+end
 end
 
-end
+
+
+
+
+# number_array.each_with_index do |digit, index|
+#   if digit === 2
+#     digit.to_s.replace "twenty"
+#   end
--- spec/number_to_words_spec.rb
+++ spec/number_to_words_spec.rb
@@ -4,6 +4,6 @@ require('pry')
 
 describe('#number_to_words') do
   it('display word of inserted number')do
-    expect(number_to_words(18)).to(eq('eighteen'))
+    expect(number_to_words(22)).to(eq([2, 2]))
   end
 end
