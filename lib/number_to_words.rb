cf2a4938d007a86d9f2da853ecb9a1134f6aedf4
Daniel Bennett and Paige Williams <daniel@>
2018-11-14 15:57:12 -0800

    numbers with two digits can be converted to word

--- lib/number_to_words.rb
+++ lib/number_to_words.rb
@@ -5,7 +5,7 @@ ones = { 0 => 'zero', 1 => 'one', 2 => 'two', 3  => 'three', 4 => 'four', 5 => '
 teens = Hash.new()
 teens = { 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen' }
 tens = Hash.new()
-tens = {2 => 'twenty', 3 => 'thirty' }
+tens = {2 => 'twenty', 3 => 'thirty', 4 => 'forty', 5 => 'fifty', 6 => 'sixty', 7 => 'seventy', 8 => 'eighty', 9 => 'ninety' }
 
 if number <= 10
 ones.fetch(number)
@@ -16,21 +16,11 @@ teens.fetch(number)
 elsif number > 20
    array = number.to_s.split("")
    number_array = array.map(&:to_i)
-   number_array.each do |digit|
-    if number_array[0] == 2 && number_array[1] < 10
-      replaced = number_array[0].to_s.replace "twenty" + ones.fetch(number_array[1])
-      #&& number_array[1].to_s.replace "one"
+  if number_array[0] < 10 && number_array[1] == 0
+   replaced = tens.fetch(number_array[0])
+  elsif number_array[0] < 10 && number_array[1] < 10
+      replaced = tens.fetch(number_array[0]) + ones.fetch(number_array[1])
+
     end
-    binding.pry
-  end
 end
 end
-
-
-
-
-
-# number_array.each_with_index do |digit, index|
-#   if digit === 2
-#     digit.to_s.replace "twenty"
-#   end
--- spec/number_to_words_spec.rb
+++ spec/number_to_words_spec.rb
@@ -4,6 +4,10 @@ require('pry')
 
 describe('#number_to_words') do
   it('display word of inserted number')do
-    expect(number_to_words(22)).to(eq([2, 2]))
+    expect(number_to_words(1)).to(eq('one'))
+  end
+
+  it('display word of inserted number with two digits')do
+    expect(number_to_words(54)).to(eq('fiftyfour'))
   end
 end
